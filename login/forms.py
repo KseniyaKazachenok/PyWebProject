a7c12d5f0e97c4337b479edea67cbb0bc2061df9
KseniyaKazachenok <ksu-90@yandex.ru>
2023-06-11 22:12:34 +0300

    Практика 2 Admin Login/Logout/Create Добавление данных

---
+++ login/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.forms import EmailField
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        fields_classes = {'username': UsernameField}
